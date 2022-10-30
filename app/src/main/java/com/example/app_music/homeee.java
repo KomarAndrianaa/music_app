8200237fef50fafda2e39907b4f195990ef1c421
Komar <komarandriana13@mhs.pelitabangsa.ac.id>
2022-10-31 00:41:58 +0700

    first commit

---
+++ app/src/main/java/com/example/app_music/homeee.java
@@ -0,0 +1,14 @@
+package com.example.app_music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homeee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homee);
+    }
+}
